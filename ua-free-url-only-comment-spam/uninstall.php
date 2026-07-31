3bf422c0784a448a94b42f885297710f84831f3d
ramirkoz <ramirua.fm@gmail.com>
2026-07-31 17:16:24 +0300

    Prefix URL-Only Comment Spam uninstall data

--- ua-free-url-only-comment-spam/uninstall.php
+++ ua-free-url-only-comment-spam/uninstall.php
@@ -7,8 +7,8 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-$settings = get_option( 'uafree_url_only_spam_settings', array() );
-if ( ! is_array( $settings ) || empty( $settings['delete_data_on_uninstall'] ) ) {
+$uafree_url_spam_settings = get_option( 'uafree_url_only_spam_settings', array() );
+if ( ! is_array( $uafree_url_spam_settings ) || empty( $uafree_url_spam_settings['delete_data_on_uninstall'] ) ) {
 	return;
 }
 
