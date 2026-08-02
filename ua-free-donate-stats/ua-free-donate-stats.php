e15d153092a5ec6d7bcfd9233aa70e32f5c6e8e2
ramirkoz <ramirua.fm@gmail.com>
2026-08-02 14:21:31 +0300

    Release Donate Stats 1.2.8 support panel update

--- ua-free-donate-stats/ua-free-donate-stats.php
+++ ua-free-donate-stats/ua-free-donate-stats.php
@@ -5,7 +5,7 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Author URI: https://uafree.org/
  * Plugin URI: https://uafree.org/ua-free-plugins/#plugin-ua-free-donate-stats
- * Version: 1.2.7
+ * Version: 1.2.8
  * Author: UA FREE
  * Text Domain: ua-free-donate-stats
  * Domain Path: /languages
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'UAFREE_DONATE_STATS_VERSION', '1.2.7' );
+define( 'UAFREE_DONATE_STATS_VERSION', '1.2.8' );
 define( 'UAFREE_DONATE_STATS_FILE', __FILE__ );
 define( 'UAFREE_DONATE_STATS_DIR', plugin_dir_path( __FILE__ ) );
 define( 'UAFREE_DONATE_STATS_URL', plugin_dir_url( __FILE__ ) );
