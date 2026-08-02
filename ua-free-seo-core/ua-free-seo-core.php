54318b928ef73a4ec7154412d88d4f931b9b1593
ramirkoz <ramirua.fm@gmail.com>
2026-08-02 13:30:23 +0300

    Release UA FREE SEO Core 2.0.8

--- ua-free-seo-core/ua-free-seo-core.php
+++ ua-free-seo-core/ua-free-seo-core.php
@@ -5,7 +5,7 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Author URI: https://uafree.org/
  * Plugin URI: https://uafree.org/ua-free-plugins/#plugin-ua-free-seo-core
- * Version: 2.0.7
+ * Version: 2.0.8
  * Author: UA FREE
  * Text Domain: ua-free-seo-core
  * Domain Path: /languages
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'UAFREE_SEO_VERSION', '2.0.7' );
+define( 'UAFREE_SEO_VERSION', '2.0.8' );
 define( 'UAFREE_SEO_FILE', __FILE__ );
 define( 'UAFREE_SEO_DIR', plugin_dir_path( __FILE__ ) );
 define( 'UAFREE_SEO_URL', plugin_dir_url( __FILE__ ) );
